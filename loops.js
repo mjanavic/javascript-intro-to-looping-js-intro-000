d161ed195c05b9db474baaed8339a2a903e811cf
Marijana Zlatkovic <m.zlatkovic@ymail.com>
2018-02-01 23:41:27 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -21,6 +21,6 @@ function doWhileLoop(array) {
 
   do {
     array.slice(1);
-  } while(array.lenth === 0 && maybeTrue() === false);
+  } while(array.length > 0 && maybeTrue());
   return array;
 }
